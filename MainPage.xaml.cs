567a91d3f281ff1b8d328b2c29e11b8f11eb0b1f
Frank <frankrod44@gmail.com>
2012-05-28 11:26:57 -0400

    built grid programmatically and set event handlers

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,15 +15,30 @@ namespace ConwayApp
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        //int n, m;
+        int N = 6, M = 10;
         SolidColorBrush alive = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
         SolidColorBrush dead = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
-
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            Button[,] button = new Button[N, M];
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                {
+                    button[i, j] = new Button();
+                    button[i, j].Width = 100;
+                    button[i, j].Height = 100;
+                    button[i, j].HorizontalAlignment = HorizontalAlignment.Left;
+                    button[i, j].VerticalAlignment = VerticalAlignment.Top;
+                    button[i, j].Click += new RoutedEventHandler(toggle);
+                    button[i, j].Margin = new Thickness(75*i, 75*j, 0, 0);
+                    
+                    this.LayoutRoot.Children.Add(button[i, j]);
+                }
+            
         }
 
         private void toggle(object sender, RoutedEventArgs e)
@@ -34,6 +49,7 @@ namespace ConwayApp
                 b.Background = dead;
             else b.Background = alive;
         }
+
 
     }
 }
