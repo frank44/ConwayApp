b511f3fa226846f62ff464d91fe7d30621a8ced0
Frank <frankrod44@gmail.com>
2012-05-28 06:05:38 -0400

    more init stuff

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace ConwayApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        //int n, m;
+        SolidColorBrush alive = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+        SolidColorBrush dead = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void toggle(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            
+            if (b.Background == alive)
+                b.Background = dead;
+            else b.Background = alive;
+        }
+
+    }
+}
